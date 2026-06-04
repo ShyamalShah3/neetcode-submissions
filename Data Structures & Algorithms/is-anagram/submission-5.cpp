c914eb3b0804b54a99d3ef84d3373715752baab3
Shyamal Shah <47264079+ShyamalShah3@users.noreply.github.com>
2026-06-03 23:50:35 -0700

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> freq;
+
+        for (char ch : s) {
+            freq[ch]++;
+        }
+
+        for (char ch : t) {
+            freq[ch]--;
+        }
+
+        for (auto [key,val] : freq) {
+            if (val != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
